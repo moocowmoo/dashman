5101c0e7f36c5b8a5ad2b1390a122574ef8dc098
moocowmoo <moocowmoo@masternode.me>
2015-09-09 07:57:15 +0000

    bugfix - exec sync from any directory

--- .dashman-functions.sh
+++ .dashman-functions.sh
@@ -153,7 +153,7 @@ _check_dashman_updates() {
         pending "sync dashman to github now?"
 
         if confirm " [${C_GREEN}y${C_NORM}/${C_RED}N${C_NORM}] $C_CYAN"; then
-            exec ${0##*/} sync
+            exec $DASHMAN_GITDIR/${0##*/} sync
         fi
         die 'Exiting.'
     fi
