3bd19e1503a6b5922ac1ea8dfa32e332aedab2d7
moocowmoo <moocowmoo@masternode.me>
2015-10-11 07:27:49 +0000

    re-enable compressed key signing support - pep8

--- lib/dashutil.py
+++ lib/dashutil.py
@@ -14,6 +14,7 @@ from pycoin.ecdsa import ellipticcurve, numbertheory
 
 
 class Bip62SigningKey(ecdsa.SigningKey):
+
     """Enforce low S values in signatures"""
 
     def sign_number(self, number, entropy=None, k=None):
@@ -37,21 +38,29 @@ def str_to_long(b):
 
 def sign_vote(votestr, mnprivkey):
     privatekey = utils.wifToPrivateKey(mnprivkey)
-    signingkey = Bip62SigningKey.from_string(privatekey.decode('hex'), curve=ecdsa.SECP256k1)
+    signingkey = Bip62SigningKey.from_string(
+        privatekey.decode('hex'),
+        curve=ecdsa.SECP256k1)
     public_key = signingkey.get_verifying_key()
     key = Key.from_text(mnprivkey)
     address = key.address(use_uncompressed=True)
     msghash = utils.double_sha256(utils.msg_magic(votestr))
-    signature = signingkey.sign_digest_deterministic(msghash, hashfunc=hashlib.sha256, sigencode=ecdsa.util.sigencode_string)
-    assert public_key.verify_digest(signature, msghash, sigdecode=ecdsa.util.sigdecode_string)
+    signature = signingkey.sign_digest_deterministic(
+        msghash,
+        hashfunc=hashlib.sha256,
+        sigencode=ecdsa.util.sigencode_string)
+    assert public_key.verify_digest(
+        signature,
+        msghash,
+        sigdecode=ecdsa.util.sigdecode_string)
     for i in range(4):
         sig = base64.b64encode(chr(27+i) + signature)
-        if verify_bitcoin_signature(generator_secp256k1, address, msghash, sig):
+        if verify_dash_signature(generator_secp256k1, address, msghash, sig):
             return sig
 
 
-def verify_bitcoin_signature(generator, address, message, signature):
-#    compressed = False
+def verify_dash_signature(generator, address, message, signature):
+    compressed = False
     G = generator
     curve = G.curve()
     order = G.order()
@@ -66,7 +75,7 @@ def verify_bitcoin_signature(generator, address, message, signature):
     if hb < 27 or hb >= 35:
         raise Exception("vmB", "Bad first byte")
     if hb >= 31:
-#        compressed = True
+        compressed = True
         hb -= 4
 
     recid = hb - 27
@@ -83,4 +92,4 @@ def verify_bitcoin_signature(generator, address, message, signature):
     inv_r = numbertheory.inverse_mod(r, order)
     Q = inv_r * (R * s + G * minus_e)
     key = Key(public_pair=(Q.x(), Q.y()), netcode='DASH')
-    return key.address(use_uncompressed=True) == address
+    return key.address(use_uncompressed=not compressed) == address
