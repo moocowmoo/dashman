a47249af79ece2203727a8ef0391354bb18724fe
moocowmoo <moocowmoo@masternode.me>
2018-01-22 08:19:55 +0000

    enh - dashvote - better vote dispersion, ui fixes

--- bin/dashvote.py
+++ bin/dashvote.py
@@ -5,6 +5,7 @@ import curses
 import datetime
 import json
 import os
+import random
 import re
 import subprocess
 import sys
@@ -14,7 +15,7 @@ import time
 import tty
 
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 git_dir = os.path.abspath(
     os.path.join(
         os.path.dirname(
@@ -29,6 +30,9 @@ sys.path.append(git_dir + '/lib')
 import dashutil
 
 
+urnd = random.SystemRandom()
+
+
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -40,10 +44,10 @@ def getch():
     return ch
 
 def urandom_int():
-    random = map(ord, os.urandom(4))
+    urandom = map(ord, os.urandom(4))
     offset = 0
     for d in range(4):
-        offset += pow(random[d], 2**d)
+        offset += pow(urandom[d], 2**d)
     return offset
 
 def random_offset(size):
@@ -53,10 +57,10 @@ def random_offset(size):
 
 def random_timestamp():
     now_epoch = int(time.time())
-    random = map(ord, os.urandom(4))
+    urandom = map(ord, os.urandom(4))
     offset = 0
     for d in range(4):
-        offset += pow(random[d], 2**d)
+        offset += pow(urandom[d], 2**d)
     return now_epoch - (offset % 86400)
 
 
@@ -115,20 +119,21 @@ def update_vote_display(win, sel_ent, vote):
         "ABSTAIN": C_YELLOW,
         '': 4
     }
-    _y = 6
+    _y = 7
+    _xoff = 12
     if vote == '':
         sel_ent += 1
-        win.move(sel_ent + _y, window_width + 7)
+        win.move(sel_ent + _y, window_width + _xoff)
         win.addstr('       ')
-        win.move(sel_ent + _y, window_width + 7)
+        win.move(sel_ent + _y, window_width + _xoff)
         win.addstr('CONFIRM', C_GREEN)
-        win.move(sel_ent + _y, window_width + 7)
+        win.move(sel_ent + _y, window_width + _xoff)
     else:
-        win.move(sel_ent + _y, window_width + 7)
+        win.move(sel_ent + _y, window_width + _xoff)
         win.addstr('       ')
-        win.move(sel_ent + _y, window_width + 7)
+        win.move(sel_ent + _y, window_width + _xoff)
         win.addstr(vote, vote_colors[vote])
-        win.move(sel_ent + _y, window_width + 7)
+        win.move(sel_ent + _y, window_width + _xoff)
 
 
 def submit_votes(win, ballot, s):
@@ -147,29 +152,50 @@ def submit_votes(win, ballot, s):
     votewin.clear()
     stdscr.move(0, 0)
 
+    votenum=0
     if votes_to_send.keys():
         if background_send:
-            deferred_votes = tempfile.NamedTemporaryFile(prefix='sending_dashvote_votes-',delete=False)
-            deferred_votes.write("#!/bin/sh\n")
-            os.chmod(deferred_votes.name, 0700)
             stdscr.addstr("writing time-randomized votes to disk\n\n", C_GREEN)
         else:
             stdscr.addstr("sending time-randomized votes\n\n", C_GREEN)
         stdscr.refresh()
 
-        # spread out over a day
+        # spread out over remaining voting duration
         total_sends = (len(votes_to_send) * len(masternodes))
-        vote_delay = total_sends > 1 and 86400/total_sends or 30
+        vote_window = int(86400*(float(days_to_finalization)-0.5))
+        randset = urnd.sample(xrange(1,vote_window), len(masternodes) * len(votes_to_send))
 
-        for vote in sorted(votes_to_send, key=lambda s: s.lower()):
-            castvote = str(votes_to_send[vote][u'vote'])
-            stdscr.addstr('  ' + vote, C_YELLOW)
-            stdscr.addstr(" --> ")
-            stdscr.addstr(castvote, castvote == 'YES' and C_GREEN or C_RED)
-            stdscr.addstr(" -- ")
-            stdscr.addstr(str(votes_to_send[vote][u'Hash']))
-            stdscr.addstr("\n")
-            for mn in sorted(masternodes):
+        vote_files = []
+        shuffled_mn_keys = masternodes.keys()
+        urnd.shuffle(shuffled_mn_keys)
+
+
+        batch_timestamp = datetime.datetime.strftime(datetime.datetime.now(),'%Y%m%d%H%M%S')
+        for mn in shuffled_mn_keys:
+
+            offsets = sorted(randset[:len(votes_to_send)])
+            del randset[:len(votes_to_send)]
+            delays = offsets[:1] + [y-x for x,y in zip(offsets, offsets[1:])]
+
+            if background_send:
+                deferred_votes = tempfile.NamedTemporaryFile(prefix=('sending_dashvote_votes-%s=' % batch_timestamp),delete=False)
+                deferred_votes.write("#!/bin/bash\n")
+                deferred_votes.write("set -x\n")
+                os.chmod(deferred_votes.name, 0700)
+                vote_files.append(deferred_votes)
+
+            shuffled_vote_keys = votes_to_send.keys()
+            urnd.shuffle(shuffled_vote_keys)
+            for vote in shuffled_vote_keys:
+                castvote = str(votes_to_send[vote][u'vote'])
+                stdscr.addstr('  ' + vote, C_YELLOW)
+                stdscr.addstr(" --> ")
+                stdscr.addstr(castvote, castvote == 'YES' and C_GREEN or C_RED)
+                stdscr.addstr(" -- ")
+                stdscr.addstr(str(votes_to_send[vote][u'Hash']))
+                stdscr.addstr("\n")
+
+                votenum += 1
                 node = masternodes[mn]
                 alias = masternodes[mn]['alias']
                 random_ts = random_timestamp()
@@ -183,35 +209,48 @@ def submit_votes(win, ballot, s):
                 command = ('%s' % dash_cli_path is not None and dash_cli_path or 'dash-cli') + ' voteraw ' + str(node['txid']) + ' ' + str(node['txout']) + ' ' + str(
                     votes_to_send[vote][u'Hash']) + ' funding ' + str(votes_to_send[vote][u'vote']).lower() + ' ' + str(random_ts) + ' ' + signature
                 if background_send:
-                    deferred_votes.write("sleep %s\n" % (vote_delay + random_offset(vote_delay)))
+                    sleeptime = delays.pop(0)
+                    deferred_votes.write("echo \"sleeping %s seconds then casting vote %s/%s\"\n" % (sleeptime,votenum,total_sends))
+                    deferred_votes.write("sleep %s\n" % sleeptime)
                     deferred_votes.write("%s\n" % (command))
-                    stdout = 'vote successfully created'
+                    #msg = 'vote successfully created - %s' % sleeptime
+                    msg = 'vote successfully created'
                 else:
                     try:
-                        stdout = run_command(command)
+                        msg = run_command(command)
                     except subprocess.CalledProcessError,e:
-                        stdout = 'error running vote command: %s' % command
+                        msg = 'error running vote command: %s' % command
                 stdscr.addstr(
-                    stdout.rstrip("\n") +
+                    msg.rstrip("\n") +
                     "\n",
-                    'successfully' in stdout and C_GREEN or C_RED)
+                    'successfully' in msg and C_GREEN or C_RED)
                 stdscr.refresh()
 
         if background_send:
-            if deferred_votes.tell() > 1 :
-                deferred_votes.write("rm -- \"$0\"\n")
-                stdout = 'sending votes in background'
-                stdscr.addstr( "\n" + stdout.rstrip("\n") + "\n", C_YELLOW )
-                stdscr.refresh()
-                exec_path = deferred_votes.name
-                deferred_votes = None
+            voter_parent = tempfile.NamedTemporaryFile(prefix=('sending_dash_votes-%s-' % batch_timestamp), delete=False)
+            voter_parent.write("#!/bin/bash\n")
+            voter_parent.write('trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT' + "\n")
+            os.chmod(voter_parent.name, 0700)
+            for deferred_votes in vote_files:
+                if deferred_votes.tell() > 1 :
+                    deferred_votes.write("rm -- \"$0\"\n")
+                    deferred_votes.close()
+                    exec_path = deferred_votes.name
+                    log_path = exec_path + '.log'
+                    voter_parent.write("%s > %s &\n" % (exec_path, log_path ))
+            voter_parent.write("wait\n")
+            exec_path = voter_parent.name
+            voter_parent.close()
+            log_path = exec_path + '.log'
+            with open(log_path, 'wb') as log:
                 p = subprocess.Popen([exec_path],
-                                     cwd="/tmp",
-                                     stdout=subprocess.PIPE,
-                                     stderr=subprocess.STDOUT)
-                stdout = 'votes being sent by: %s, pid %s' % ( exec_path, p.pid)
-                stdscr.addstr( "\n" + stdout.rstrip("\n") + "\n", C_CYAN )
-                stdscr.refresh()
+                                     cwd="/tmp", stdout=log, stderr=log)
+            msg = 'sending votes in background'
+            stdscr.addstr( "\n" + msg + "\n", C_YELLOW )
+            stdscr.refresh()
+            msg = 'votes being sent by: %s, pid %s' % ( voter_parent.name, p.pid)
+            stdscr.addstr( "\n" + msg + "\n", C_CYAN )
+            stdscr.refresh()
 
     stdscr.addstr("\nHit any key to exit." + "\n", C_GREEN)
     stdscr.refresh()
@@ -237,6 +276,8 @@ def main(screen):
     global max_yeacount_len
     global max_naycount_len
     global max_percentage_len
+    global max_needed_len
+    global days_to_finalization
     global ballot_entries
     global votewin
     global masternodes
@@ -282,13 +323,18 @@ def main(screen):
     loadwin = curses.newwin(40, 40, 1, 2)
 
     loadwin.addstr(1, 2, 'dashvote version: ' + version, C_CYAN)
-    loadwin.addstr(2, 2, 'loading votes. please wait...', C_GREEN)
-    loadwin.refresh()
 
-    mncount = int(run_dash_cli_command('masternode count'))
+    mncount = int(run_dash_cli_command('masternode count enabled'))
     block_height = int(run_dash_cli_command('getblockcount'))
-    days_to_next_cycle = (16616 - (block_height % 16616)) / 576.0
+    blocks_to_next_cycle = (16616 - (block_height % 16616))
+    next_cycle_epoch = int(int(time.time()) + (157.5 * blocks_to_next_cycle))
+    days_to_next_cycle = blocks_to_next_cycle / 576.0
     days_to_finalization = days_to_next_cycle - 3
+
+    loadwin.addstr(2, 2, '{:0.2f} days remaining to vote'.format(days_to_finalization), C_GREEN)
+    loadwin.addstr(3, 2, 'loading votes. please wait...', C_GREEN)
+    loadwin.refresh()
+    time.sleep(1)
 
     # get ballot
     ballots = json.loads(run_dash_cli_command('gobject list all'))
@@ -305,36 +351,55 @@ def main(screen):
         if str(go_type) == 'watchdog':
             continue
 
+        if int(go_data["type"]) == 2:
+            continue
+
         if int(go_data[u'end_epoch']) < int(time.time()):
+            continue
+
+        if (ballots[entry][u'NoCount'] - ballots[entry][u'YesCount']) > mncount/10:
+            continue
+
+        if int(go_data[u'end_epoch']) < next_cycle_epoch:
             continue
 
         ballots[entry][u'vote'] = 'SKIP'
         ballots[entry][u'votes'] = json.loads(run_dash_cli_command('gobject getvotes %s' % entry))
+
         ballot[entry] = ballots[entry]
 
-    ballot_entries = sorted(ballot, key=lambda s: ballot[s]['proposal']['start_epoch'])
-    votecount = len(ballot_entries)
+    votecount = len(ballot)
     max_proposal_len = 0
     max_yeacount_len = 0
     max_naycount_len = 0
     max_percentage_len = 0
-    for entry in ballot_entries:
+    max_needed_len = 0
+    for entry in ballot:
         yeas = ballot[entry][u'YesCount']
         nays = ballot[entry][u'NoCount']
         name = ballot[entry]['proposal'][u'name']
+        threshold = mncount/10
         percentage = "{0:.1f}".format(
             (float((yeas + nays)) / float(mncount)) * 100)
+        votes_needed = (threshold) - (yeas - nays)
         ballot[entry][u'vote_turnout'] = percentage
+        ballot[entry][u'total_votes'] = yeas + nays
+        ballot[entry][u'votes_needed'] = votes_needed
         ballot[entry][u'vote_threshold'] = (
-            yeas + nays) > mncount/10 and True or False
+            yeas + nays) > threshold and True or False
         ballot[entry][u'vote_passing'] = (
-            yeas - nays) > mncount/10 and True or False
+            yeas - nays) > threshold and True or False
+        ballot[entry][u'voted_down'] = (
+            nays - yeas) > threshold and True or False
         max_proposal_len = max(
             max_proposal_len,
             len(name))
+        max_needed_len = max(max_needed_len, len(str(votes_needed)))
         max_yeacount_len = max(max_yeacount_len, len(str(yeas)))
         max_naycount_len = max(max_naycount_len, len(str(nays)))
         max_percentage_len = max(max_percentage_len, len(str(percentage)))
+
+    ballot_entries = sorted(ballot, key=lambda s: ballot[s]['votes_needed'], reverse=False)
 
     # extract mnprivkey,txid-txidx from masternode.conf
     masternodes = {}
@@ -407,9 +472,9 @@ def main(screen):
 
     loadwin.erase()
     window_width = 35
-    content_width = max_proposal_len + max_percentage_len + max_yeacount_len + max_naycount_len
+    content_width = max_proposal_len + max_percentage_len + max_yeacount_len + max_needed_len + max_naycount_len
     window_width = max(window_width, content_width + 3 )
-    votewin = curses.newwin(votecount + 9, window_width + 17, 1, 2)
+    votewin = curses.newwin(votecount + 10, window_width + 21, 1, 2)
     votewin.keypad(1)
     votewin.border()
 
@@ -417,24 +482,26 @@ def main(screen):
     votewin.addstr(
         2,
         2,
-        'use arrow keys to set votes for %s masternodes' %
-        len(masternodes),
-        C_YELLOW)
+        'use arrow keys to set votes for {:d} masternodes.  Total nodes: {:d}'.format(len(masternodes), mncount), C_YELLOW)
     votewin.addstr(3, 2, 'hit enter on CONFIRM to vote - q to quit', C_YELLOW)
     votewin.addstr(4, 3, '*', C_GREEN)
     votewin.addstr(4, 4, '/', C_CYAN)
     votewin.addstr(4, 5, '*', C_RED)
     votewin.addstr(4, 7, '== previously voted on proposal (yes/no)', C_YELLOW)
-    _y = 5
+    votewin.addstr(6, 4, 'proposal                                  yeas/nays needs turnout vote', C_CYAN)
+
+    _y = 6
     for entry in ballot_entries:
         _y += 1
         x = 4
         name = ballot[entry]['proposal'][u'name']
+        amount = ballot[entry]['proposal'][u'payment_amount']
         yeas = ballot[entry][u'YesCount']
         nays = ballot[entry][u'NoCount']
         percentage = ballot[entry][u'vote_turnout']
         passing = ballot[entry][u'vote_passing']
         threshold = ballot[entry][u'vote_threshold']
+        votes_needed = ballot[entry][u'votes_needed']
         if ballot[entry][u'previously_voted'] > 0:
             direction = ballot[entry][u'previously_voted']
             votewin.addstr(_y, x-1, '*', direction == 1 and C_GREEN or C_RED)
@@ -452,10 +519,16 @@ def main(screen):
         votewin.addstr(str(yeas), C_GREEN)
         votewin.addstr('/', C_CYAN)
         votewin.addstr(str(nays), C_RED)
-        votewin.addstr(' ')
 
         for x in range(max_naycount_len - len(str(nays))):
             votewin.addstr(' ')
+
+        for x in range(max_needed_len - len(str(votes_needed))):
+            votewin.addstr(' ')
+
+        votewin.addstr('   ')
+        votewin.addstr(str(votes_needed), C_CYAN)
+        votewin.addstr('   ')
 
         for x in range(max_percentage_len - len(str(percentage))):
             votewin.addstr(' ')
@@ -466,10 +539,10 @@ def main(screen):
         votewin.addstr('SKIP', C_CYAN)
     votewin.addstr(
         _y + 2,
-        window_width + 7,
+        window_width + 12,
         'confirm',
         C_YELLOW)
-    votewin.move(0 + 6, window_width + 7)
+    votewin.move(0 + 7, window_width + 12)
 
     votewin.refresh()
 
